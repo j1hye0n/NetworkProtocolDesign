9ce59bc7516897743eba7d4230331892d9c4fd4a
j1hye0n <j1hyeon@sookmyung.ac.kr>
2025-05-28 21:03:43 +0900

    base staion
    add destID

--- BaseStation/baseCode_Capstone/L3_FSMmain.cpp
+++ BaseStation/baseCode_Capstone/L3_FSMmain.cpp
@@ -86,23 +86,12 @@ void L3_FSMrun(void)
     switch (main_state)
     {
         case L3STATE_IDLE: //IDLE state description
-
-            // 주기적 DATA 송신을 위한 timer 작동
-            // 처음 PDU 셋팅
-            if (!L3_timer_getTimerStatus())
-            {
-                pc.printf("sending data , %i\n",L3_timer_getTimerStatus());
-                strcpy((char*) originalWord, "Sending DATA\n\r");
-                L3_event_setEventFlag(L3_event_dataToSend);
-                L3_timer_startTimer();
-            }
-
-            else if (L3_event_checkEventFlag(L3_event_msgRcvd)) //if data reception event happens (event a)
+            if (L3_event_checkEventFlag(L3_event_msgRcvd)) //if data reception event happens (event a)
             {
                 //Retrieving data info.
                 uint8_t* dataPtr = L3_LLI_getMsgPtr();
                 // uint8_t size = L3_LLI_getSize();
-                uint8_t rssi = L3_LLI_getRssi();    //Rssi&ID variables are added date.05/13
+                // uint8_t rssi = L3_LLI_getRssi();    //Rssi&ID variables are added date.05/13
                 uint8_t srcId = L3_LLI_getSrcId();
 
                 // debug("\n -------------------------------------------------\nRCVD MSG : %s (length:%i)\n -------------------------------------------------\n", 
@@ -115,6 +104,7 @@ void L3_FSMrun(void)
                         pc.printf("Request from UE 221 to be connected.\n\r");
 
                         strcpy((char*) originalWord, "ACCEPT\n\r"); //action 2
+                        myDestId = srcId;
                         L3_event_setEventFlag(L3_event_dataToSend);
                         pc.printf("Tried to Send ACCEPT to UE 221.\n\r");
                     }
@@ -140,11 +130,12 @@ void L3_FSMrun(void)
 
                 //PDU 기본값으로 셋팅
                 strcpy((char*) originalWord, "Sending DATA\n\r");
+                myDestId = 255;
                 
                 L3_event_clearEventFlag(L3_event_dataToSend);
             }
             
-            else if (L3_event_checkEventFlag(L3_event_arqTimeout))
+            if (L3_event_checkEventFlag(L3_event_arqTimeout))
             {   
                 // 일정 시간(Timer)마다 PDU(기지국 DATA) 재전송
                 // L3_timer_stopTimer();
@@ -153,6 +144,15 @@ void L3_FSMrun(void)
                 L3_timer_startTimer();
 
                 L3_event_clearEventFlag(L3_event_arqTimeout);
+            }
+            else if (!L3_timer_getTimerStatus())
+            {
+                // 주기적 DATA 송신을 위한 timer 작동
+                // 처음 PDU 셋팅
+                pc.printf("sending data , %i\n",L3_timer_getTimerStatus());
+                strcpy((char*) originalWord, "Sending DATA\n\r");
+                L3_event_setEventFlag(L3_event_dataToSend);
+                L3_timer_startTimer();
             }
             break;
 
